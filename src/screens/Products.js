39faecf2d5ceb7b71b5f9fca233aaf9417ba071a
sebidev999 <38624391+sebidev999@users.noreply.github.com>
2021-01-02 23:32:39 -0800

    feat: products screen

---
+++ src/screens/Products.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchProducts } from '../redux/reducers/product';
+import { addToCart } from '../redux/reducers/cart';
+import { productsSelector } from '../redux/selectors';
+
+import Header from '../components/common/Header';
+import CartButton from '../components/common/CartButton'
+import ProductRow from '../components/product/ProductRow';
+
+export const Products = () => {
+    const dispatch = useDispatch();
+    const products = useSelector(productsSelector);
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
+
+    const onProductAdd = (product) => {
+        dispatch(addToCart(product.sku));
+    }
+
+    return (
+        <div className="container">
+            <Header title="Products" right={<CartButton />}></Header>
+            
+            {products?.map(p => <ProductRow product={p} key={p.sku} onProductAdd={() => onProductAdd(p)} />)}
+        </div>
+    );
+}
