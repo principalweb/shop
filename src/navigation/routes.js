d718ad4bad2db1557f5e280f6986aa78dceb3e8d
sebidev999 <38624391+sebidev999@users.noreply.github.com>
2021-01-11 16:37:34 -0800

    feat: checkout page

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import { Carts } from '../screens/Carts'
 import { Products } from '../screens/Products'
 
 export const BASE_ROUTES = {
@@ -12,6 +13,7 @@ const BaseRoutes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path={BASE_ROUTES.products} component={Products} />
+      <Route exact path={BASE_ROUTES.carts} component={Carts} />
     </Switch>
   </BrowserRouter>
 )
